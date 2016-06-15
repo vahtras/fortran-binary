cff350d739cc7531ff1680b9bd6c75fad62bcf31
Olav Vahtras <vahtras@kth.se>
2016-06-15 02:38:09 +0200

    rename

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author_email="olav.vahtras@gmail.com",
     version="1.0",
     url="https://github.com/vahtras/fortran-binary",
-    py_modules=["unformatted"],
+    py_modules=["fortran_binary"],
     scripts=["fb.py"],
     install_requires=["numpy"],
 )
