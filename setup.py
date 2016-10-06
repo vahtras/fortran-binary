a2fe8dd13a129a45f815de18d21ead5a66fd409d
Olav Vahtras <vahtras@kth.se>
2016-10-06 13:32:52 +0200

    Add pypi version number

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
+from fortran_binary import __version__
 
 setup(
     name="fortran-binary",
     author="Olav Vahtras",
     author_email="olav.vahtras@gmail.com",
-    version="1.0",
+    version=__version__,
     url="https://github.com/vahtras/fortran-binary",
     py_modules=["fortran_binary"],
     scripts=["forbin"],
