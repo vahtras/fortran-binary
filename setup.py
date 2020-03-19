8d8b67660c7283929b313949736472d5ccd383a1
Olav Vahtras <vahtras@kth.se>
2020-03-19 13:31:18 +0100

    include test file in sdist

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email="olav.vahtras@gmail.com",
     version=__version__,
     url="https://github.com/vahtras/fortran-binary",
-    py_modules=["fortran_binary"],
+    py_modules=["fortran_binary", "test_fb"],
     scripts=["forbin"],
     install_requires=["numpy"],
     description="Utility for reading Fortran binary files",
