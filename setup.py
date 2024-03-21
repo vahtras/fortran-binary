57bbc29d1278b870865bfc954fc94ec7bd92866f
Olav Vahtras <vahtras@kth.se>
2024-03-22 00:50:03 +0100

    fix deps

--- setup.py
+++ setup.py
@@ -1,18 +1,2 @@
 from setuptools import setup
 setup()
-
-"""
-from fortran_binary import __version__
-
-setup(
-    name="fortran-binary",
-    author="Olav Vahtras",
-    author_email="olav.vahtras@gmail.com",
-    version=__version__,
-    url="https://github.com/vahtras/fortran-binary",
-    py_modules=["fortran_binary", "test_fb"],
-    scripts=["forbin"],
-    install_requires=["numpy"],
-    description="Utility for reading Fortran binary files",
-)
-"""
